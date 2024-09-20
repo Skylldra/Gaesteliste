36837f691bbcb45a9edb93232fc6ddd5c81b2a71
Skylldra <kily1010mc@gmail.com>
2024-09-20 22:55:29 +0200

    Update server.js
    Umstellung von Heroku auf Render

--- server.js
+++ server.js
@@ -2,25 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// CORS, um Zugriff vom Client zu ermöglichen
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-// Post-Route zum Empfang der Nachricht
 app.post('/submit-message', (req, res) => {
     const { name, message } = req.body;
-
     const textToSave = `"${message}" von ${name}\n`;
 
-    // Prüfe, ob die Datei existiert, wenn nicht, wird sie erstellt
     fs.appendFile('Nachrichten.txt', textToSave, (err) => {
         if (err) {
             console.error('Fehler beim Schreiben in die Datei:', err);
@@ -30,6 +19,6 @@ app.post('/submit-message', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server läuft auf http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
 });
